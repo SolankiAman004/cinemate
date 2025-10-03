c49dccf1c2d818c5720b4cc51adf2f95e92df548
Aman <Aman.Solanki@imsplgroup.com>
2025-10-03 16:56:51 +0530

    error resloved

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,14 +8,14 @@ export const Search = ({apiPath}) => {
   const queryTerm = searchParams.get("q");
   const { data: movies } = useFetch(apiPath, queryTerm);
 
-  useTitle(`Search result for ${queryTerm}`);
+  const pageTitle = useTitle(`Search result for ${queryTerm}`);
 
   return (
     <main>
       <section className="py-7">
         <p className="text-3xl text-gray-700 dark:text-white">{ movies.length === 0 ? `No result found for '${queryTerm}'` : `Result for '${queryTerm}'` }</p>
       </section>
-      <section className="max-w-7xl mx-auto">
+      <section className="max-w-7xl mx-auto mt-1">
         <div className="flex justify-start flex-wrap space-x-4 space-y-4">       
           { movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
